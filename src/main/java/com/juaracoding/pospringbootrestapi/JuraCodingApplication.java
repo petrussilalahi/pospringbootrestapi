fcd3421f53e33ebc2f5c02a894e27fd33d6bf27b
petrus766hi <petrus.sigiro@gmail.com>
2023-07-22 22:04:32 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/pospringbootrestapi/JuraCodingApplication.java
@@ -0,0 +1,13 @@
+package com.juaracoding.pospringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuraCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuraCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juaracoding/pospringbootrestapi/JuraCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.pospringbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuraCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
